d477ce9f57b614664436a3b857bb8bea2bccb834
Thiago amaro <thiagoamaro2431@gmail.com>
2023-11-22 09:50:56 -0300

    Añadir rutas al inicio

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import projectRoutes from './routes/user.routes.js'
 import comRoutes from './routes/comercio.routes.js'
 import cors  from "cors";
+import walletRoutes  from "./routes/wallet.routes.js";
+import cuponRoutes  from "./routes/cupon.routes.js";
+import catRoutes  from "./routes/category.reoutes.js";
 
 const app = express();
 
@@ -14,4 +17,7 @@ app.get('/', (req,res) =>{
 })
 app.use(projectRoutes);
 app.use(comRoutes);
+app.use(walletRoutes);
+app.use(cuponRoutes);
+app.use(catRoutes);
 export default app;
