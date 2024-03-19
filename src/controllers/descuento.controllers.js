8091efc32041d7b1e009c7e33981dc7b9f9d5071
Thiago amaro <thiagoamaro2431@gmail.com>
2024-03-19 04:12:12 -0300

    added routes

--- src/controllers/descuento.controllers.js
+++ src/controllers/descuento.controllers.js
@@ -1,3 +1,5 @@
+import { Categoria } from "../models/categoria.js"
+import { Comercio } from "../models/comercio.js"
 import { Descuento } from "../models/descuento.js"
 
 export const getDescuentos = async (req,res)=>{
@@ -16,6 +18,32 @@ export const getDescuento = async(req,res) =>{
           where:{id}
        })
        if(!descuento) return res.status(404).json({message:"El descuento no existe"})
+       res.json(descuento)
+    } catch (error) {
+       return res.status(500).json({message: error.message})
+    }
+};
+export const getDescuentoXComercio = async(req,res) =>{
+    try {
+       const {comercioId} = req.params
+       const descuento = await Descuento.findAll({
+          where:{comercioId},
+          include: [{ model: Comercio }],
+       })
+       if(!descuento) return res.status(404).json({message:"No existen descuentos"})
+       res.json(descuento)
+    } catch (error) {
+       return res.status(500).json({message: error.message})
+    }
+};
+export const getDescuentoXCategoria = async(req,res) =>{
+    try {
+       const {categoriumId} = req.params
+       const descuento = await Descuento.findAll({
+          where:{categoriumId},
+          include: [{ model: Categoria }],
+       })
+       if(!descuento) return res.status(404).json({message:"No existen descuentos"})
        res.json(descuento)
     } catch (error) {
        return res.status(500).json({message: error.message})
--- src/routes/descuento.routes.js
+++ src/routes/descuento.routes.js
@@ -1,5 +1,5 @@
 import { Router} from "express";
-import {getDescuentos,getDescuento,createDescuento,updateDescuento,deleteDescuento} from "../controllers/descuento.controllers.js"
+import {getDescuentos,getDescuento,createDescuento,updateDescuento,deleteDescuento, getDescuentoXComercio, getDescuentoXCategoria} from "../controllers/descuento.controllers.js"
 
 const router = Router();
 
@@ -9,6 +9,8 @@ router.post('/descuento', createDescuento )
 router.put('/descuento/:id',updateDescuento) 
 router.delete('/descuento/:id',deleteDescuento)
 router.get('/descuento/:id',getDescuento)
+router.get('/descuentoxcomercio/:comercioId',getDescuentoXComercio)
+router.get('/descuentoxcat/:categoriumId',getDescuentoXCategoria)
 
 
 
